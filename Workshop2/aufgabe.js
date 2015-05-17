6c93169407af2032d10743a6d5b89c72177b1bac
nbastian <nico.bastian@smail.fh-koeln.de>
2015-05-18 00:13:57 +0200

    a

---
+++ Workshop2/aufgabe.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+
+var bodyParser = require('body-parser');
+//Parser-Objekt anlegen
+var jsonParser = bodyParser.json();
+
+var userliste = [{name: 'Hier kommt der Name', nummer: 'Hier kommt die Nummer'}];
+
+//Routing einer GET Methode auf die Ressource foo/beliebige ID
+app.get('/user/:id', function(req, res) {
+    res.status('200');
+    res.send('Hallo ' + req.params.id + ', schön dass Sie uns beehren. :-)');
+});
+
+//Routing einer POST Methode auf die Ressource foo
+app.post('/verwaltung', jsonParser, function(req, res) {
+    userliste.push(req.body);
+    res.status('200').send('User wurde der Liste hinzugefügt');
+});
+
+app.get('/verwaltung', jsonParser, function(req, res) {
+    res.status('200').send(userliste);
+});
+
+
+app.listen(1337);
