8a3b89c1c03d74dafc971a05ad63696be318d418
Christian Franke <info@franky.ws>
2015-06-06 12:56:04 +0200

    Test commit

--- Workshop2/aufgabe.js
+++ Workshop2/aufgabe.js
@@ -1,20 +1,31 @@
+/**
+ * Module dependencies.
+ */
+
+// include express module
 var express = require('express');
-var app = express();
-
-
 var bodyParser = require('body-parser');
-//Parser-Objekt anlegen
+
+// init em up
+var app = express();
 var jsonParser = bodyParser.json();
 
-var userliste = [{name: 'Hier kommt der Name', nummer: 'Hier kommt die Nummer'}];
 
-//Routing einer GET Methode auf die Ressource foo/beliebige ID
+// static userList
+var userliste = [
+	{
+		name: 'Hier kommt der Name', 
+		nummer: 'Hier kommt die Nummer'
+	}
+];
+
+// Routing einer GET Methode auf die Ressource foo/beliebige ID
 app.get('/user/:id', function(req, res) {
     res.status('200');
     res.send('Hallo ' + req.params.id + ', schön dass Sie uns beehren. :-)');
 });
 
-//Routing einer POST Methode auf die Ressource foo
+// Routing einer POST Methode auf die Ressource foo
 app.post('/verwaltung', jsonParser, function(req, res) {
     userliste.push(req.body);
     res.status('200').send('User wurde der Liste hinzugefügt');
@@ -25,6 +36,11 @@ app.get('/verwaltung', jsonParser, function(req, res) {
 });
 
 
-app.listen(1337);
-
-// test kommentar
+// 
+app.listen(1337, function () {
+	var $srv = server.address();
+	var host = $srv.address;
+	var port = $srv.port;
+	
+	console.log('App listening at http://%s:%s', host, port);
+});
