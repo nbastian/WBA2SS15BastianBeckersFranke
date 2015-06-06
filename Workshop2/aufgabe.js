30df30c1adadd1f958f66e9b673a71f43faafed2
Christian Franke <info@franky.ws>
2015-06-06 13:17:16 +0200

    add comments

--- Workshop2/aufgabe.js
+++ Workshop2/aufgabe.js
@@ -5,10 +5,17 @@
 // include express module
 var express = require('express');
 var bodyParser = require('body-parser');
+var redis = require('redis').createClient();
 
 // init em up
 var app = express();
 var jsonParser = bodyParser.json();
+
+// redis onError
+redis.on('error', function (err) {
+	console.log(err);
+});
+
 
 
 // static userList
@@ -21,26 +28,25 @@ var userliste = [
 
 // Routing einer GET Methode auf die Ressource foo/beliebige ID
 app.get('/user/:id', function(req, res) {
-    res.status('200');
+    res.status(200);
     res.send('Hallo ' + req.params.id + ', schön dass Sie uns beehren. :-)');
 });
 
 // Routing einer POST Methode auf die Ressource foo
 app.post('/verwaltung', jsonParser, function(req, res) {
     userliste.push(req.body);
-    res.status('200').send('User wurde der Liste hinzugefügt');
+    res.status(200).send('User wurde der Liste hinzugefügt');
 });
 
 app.get('/verwaltung', jsonParser, function(req, res) {
-    res.status('200').send(userliste);
+    res.status(200).send(userliste);
 });
 
 
-// 
-app.listen(1337, function () {
-	var $srv = server.address();
-	var host = $srv.address;
-	var port = $srv.port;
+// Start server listening..
+var server = app.listen(1337, function () {
+	var host = server.address().address;
+	var port = server.address().port;
 	
 	console.log('App listening at http://%s:%s', host, port);
 });
