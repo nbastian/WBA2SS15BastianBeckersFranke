e00725cd3c04373c4378e930ac456b0d009ccd90
Christian Franke <info@franky.ws>
2015-06-21 17:55:45 +0200

    new console :) fixed

--- Dienstgeber/aufgabe.js
+++ Dienstgeber/aufgabe.js
@@ -37,12 +37,13 @@ app.use(bodyParser.json({ extended: true }));
 // environments
 app.set('port', process.env.PORT || 1337);
 var userlistObj = 'userlist';
+var adminlistObj = 'adminlist';
 var eventObj = 'events';
 
 // redis error handling
 redis.on('error', function (err) {
     console.log(err);
-    process.exit(1);
+    process.exit(1);  
 });
 
 // static assets (html-files)
@@ -60,15 +61,13 @@ app.route('/initdemo').get(function(req, res) {
 			id: 1,
 			username: 'Testuser1',
 			email: 'fh@franky.ws',
-			password: '7288edd0fc3ffcbe93a0cf06e3568e28521687bc', // test123
-			isAdmin: true
+			password: '7288edd0fc3ffcbe93a0cf06e3568e28521687bc' // test123
 		},
 		{
 			id: 2,
 			username: 'Testuser2',
 			email: 'info@franky.ws',
-			password: '7288edd0fc3ffcbe93a0cf06e3568e28521687bc', // test123
-			isAdmin: false
+			password: '7288edd0fc3ffcbe93a0cf06e3568e28521687bc' // test123
 		}
 	]));
 	
@@ -91,14 +90,14 @@ app.route('/user')
         });
     })
     
-    
+      
     .post(function(req, res) {
         // save new user in db
         
-        redis.get(userlistObj, function (err, obj) {
+        redis.get(userlistObj, function (err, obj) { 
             // get old list
             var userList = JSON.parse(obj)
-                newId = userList.length;
+                newId = userList.length + 1;
             
             
             // todo: check for valid inputs
@@ -109,8 +108,7 @@ app.route('/user')
                 id: newId,
                 username: req.body.username,
                 email: req.body.email,
-                password: sha1sum(req.body.password), // never save password plaintext! todo: salt!
-                isAdmin: req.body.isAdmin == 1 // todo: isAdmin
+                password: sha1sum(req.body.password) // never save password plaintext! todo: salt!
             });
             
             // save list
@@ -133,14 +131,12 @@ app.route('/user/:id([0-9]+)')
         // return single user
                 
         redis.get(userlistObj, function (err, obj) {
-            var userList = parseJsonList(obj);
+            var userList = parseJsonList(obj),
+            	user = userList.filter(function(el) {
+	                return el.id == req.params.id
+	            });
             
-            for (var i in userList) {
-                if (userList[i] == req.params.id) {
-                    res.json(userList[i]);
-                    break;
-                }
-            }
+            res.json(user[0] || []);
         });
     })
     
@@ -152,16 +148,13 @@ app.route('/user/:id([0-9]+)')
             var userList = JSON.parse(obj);
             
             for (var i in userList) {
-                if (userList[i] == req.params.id) {
+                if (userList[i].id == req.params.id) {
                     
                     // todo: check for valid inputs
                                         
-                    userList[i] = {
-                        username: req.body.id,
-                        email: req.body.email,
-                        password: sha1sum(req.body.password), // never save password plaintext! todo: salt!
-                        isAdmin: req.body.isAdmin == 1 // todo: isAdmin
-                    };
+                    userList[i].username = req.body.username;
+                    userList[i].email = req.body.email;
+                    if (req.body.password) userList[i].password = sha1sum(req.body.password);
                     
                     break;
                 }
@@ -185,7 +178,7 @@ app.route('/user/:id([0-9]+)')
             
             // filter userlist..
             userList = userList.filter(function(el) {
-                return el.id !== req.params.id
+                return el.id != req.params.id
             });
                                 
             // save list
@@ -197,6 +190,122 @@ app.route('/user/:id([0-9]+)')
             });
         });
     });
+
+
+// endpoint for getting and setting adminuser
+app.route('/adminuser')
+    
+    .get(function(req, res) {
+        // get userlist from db
+        
+        redis.get(adminlistObj, function (err, obj) {
+	        userObj = parseJsonList(obj);
+			
+            res.json(userObj);
+        });
+    })
+    
+    
+    .post(function(req, res) {
+        // save new user in db
+        
+        redis.get(adminlistObj, function (err, obj) {
+            // get old list
+            var userList = JSON.parse(obj)
+                newId = userList.length + 1;
+            
+            
+            // todo: check for valid inputs
+            
+            
+            // push new user
+            userList.push({
+                id: newId,
+                username: req.body.username,
+                email: req.body.email,
+                password: sha1sum(req.body.password) // never save password plaintext! todo: salt!
+            });
+            
+            // save list
+            redis.set(adminlistObj, JSON.stringify(userList));
+            
+            // output
+            res.json({ 
+                success: true,
+                newId: newId 
+            });
+        });
+    });
+
+
+// endpoint for existing user
+// only with numberic user-id!
+app.route('/adminuser/:id([0-9]+)')
+
+    .get(function(req, res) {
+        // return single user
+                
+        redis.get(adminlistObj, function (err, obj) {
+            var userList = parseJsonList(obj),
+            	user = userList.filter(function(el) {
+	                return el.id == req.params.id
+	            });
+            
+            res.json(user[0] || []);
+        });
+    })
+    
+    
+    .put(function(req, res) {
+        // update single user
+                
+        redis.get(adminlistObj, function (err, obj) {
+            var userList = JSON.parse(obj);
+            
+            for (var i in userList) {
+                if (userList[i].id == req.params.id) {
+                    
+                    // todo: check for valid inputs
+                                        
+                    userList[i].username = req.body.username;
+                    userList[i].email = req.body.email;
+                    if (req.body.password) userList[i].password = sha1sum(req.body.password);
+                    
+                    break;
+                }
+            }
+                                
+            // save list
+            redis.set(adminlistObj, JSON.stringify(userList));
+            
+            // output
+            res.json({ 
+                success: true
+            });
+        });
+    })
+    
+    .delete(function(req, res) {
+        // delete single user
+                
+        redis.get(adminlistObj, function (err, obj) {
+            var userList = JSON.parse(obj);
+            
+            // filter userlist..
+            userList = userList.filter(function(el) {
+                return el.id != req.params.id
+            });
+                                
+            // save list
+            redis.set(adminlistObj, JSON.stringify(userList));
+            
+            // output
+            res.json({ 
+                success: true
+            });
+        });
+    });
+
 
 
 
