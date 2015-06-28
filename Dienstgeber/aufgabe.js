701201d7d029124db078143da6c3e659848a2c06
Christian Franke <info@franky.ws>
2015-06-28 16:34:17 +0200

    tab -> space convert

--- Dienstgeber/aufgabe.js
+++ Dienstgeber/aufgabe.js
@@ -20,13 +20,13 @@ var sha1sum = function(input) {
 }
 
 var parseJsonList = function(jsonString) {
-	try {
-	    jsonObj = JSON.parse(jsonString);
-	} catch (e) {
-	    jsonObj = [];
-	}
-	
-	return jsonObj;
+    try {
+        jsonObj = JSON.parse(jsonString);
+    } catch (e) {
+        jsonObj = [];
+    }
+    
+    return jsonObj;
 }
 
 // init em up
@@ -57,57 +57,57 @@ app.use(express.static('public'));
 
 // Objekte erstellen und Demodaten in DB legen
 app.route('/initdemo').get(function(req, res) {
-	// User
-	redis.set(userlistObj, JSON.stringify([
-		{
-			id: 1,
-			username: 'Testuser1',
-			email: 'fh@franky.ws',
-			password: '7288edd0fc3ffcbe93a0cf06e3568e28521687bc' // test123
-		},
-		{
-			id: 2,
-			username: 'Testuser2',
-			email: 'info@franky.ws',
-			password: '7288edd0fc3ffcbe93a0cf06e3568e28521687bc' // test123
-		}
-	]));
-	
-	// Adminuser
-	redis.set(adminlistObj, JSON.stringify([
-		{
-			id: 1,
-			username: 'Franky',
-			email: 'franky@pollerwiesen.org',
-			password: '7288edd0fc3ffcbe93a0cf06e3568e28521687bc' // test123
-		}
-	]));
-	
-	// Veranstaltungen
-	redis.set(eventObj, JSON.stringify([
-		{
-			id: 1,
-			name: 'PollerWiesen',
-			dateStart: moment('2015-08-15 13:00').format('X'),
-			dateEnd: moment('2015-08-15 18:00').format('X')
-		}
-	]));
-	
-	// Organisationen
-	redis.set(organizerObj, JSON.stringify([
-		{
-			id: 1,
-			name: 'PollerWiesen GmbH',
-			user: [1, 2],
-			events: [1],
-			adminId: 1
-		}
-	]));
-	
-	res.json({
-		success: true,
-		msg: 'Demodata filled successful.'
-	});
+    // User
+    redis.set(userlistObj, JSON.stringify([
+        {
+            id: 1,
+            username: 'Testuser1',
+            email: 'fh@franky.ws',
+            password: '7288edd0fc3ffcbe93a0cf06e3568e28521687bc' // test123
+        },
+        {
+            id: 2,
+            username: 'Testuser2',
+            email: 'info@franky.ws',
+            password: '7288edd0fc3ffcbe93a0cf06e3568e28521687bc' // test123
+        }
+    ]));
+    
+    // Adminuser
+    redis.set(adminlistObj, JSON.stringify([
+        {
+            id: 1,
+            username: 'Franky',
+            email: 'franky@pollerwiesen.org',
+            password: '7288edd0fc3ffcbe93a0cf06e3568e28521687bc' // test123
+        }
+    ]));
+    
+    // Veranstaltungen
+    redis.set(eventObj, JSON.stringify([
+        {
+            id: 1,
+            name: 'PollerWiesen',
+            dateStart: moment('2015-08-15 13:00').format('X'),
+            dateEnd: moment('2015-08-15 18:00').format('X')
+        }
+    ]));
+    
+    // Organisationen
+    redis.set(organizerObj, JSON.stringify([
+        {
+            id: 1,
+            name: 'PollerWiesen GmbH',
+            user: [1, 2],
+            events: [1],
+            adminId: 1
+        }
+    ]));
+    
+    res.json({
+        success: true,
+        msg: 'Demodata filled successful.'
+    });
 });
 
 // endpoint for getting and setting user
@@ -117,8 +117,8 @@ app.route('/user')
         // get userlist from db
         
         redis.get(userlistObj, function (err, obj) {
-	        userObj = parseJsonList(obj);
-			
+            userObj = parseJsonList(obj);
+            
             res.status(200).json(userObj);
         });
     })
@@ -130,11 +130,11 @@ app.route('/user')
         redis.get(userlistObj, function (err, obj) { 
             // get old list
             var userList = JSON.parse(obj)
-			var lastId = 0;
-			
+            var lastId = 0;
+            
             for (var i in userList) {
                 if (userList[i].id > lastId) {
-	                // get last id + 1
+                    // get last id + 1
                     lastId = parseInt(userList[i].id);
                 }
             }
@@ -175,9 +175,9 @@ app.route('/user/:id([0-9]+)')
                 
         redis.get(userlistObj, function (err, obj) {
             var userList = parseJsonList(obj),
-            	user = userList.filter(function(el) {
-	                return el.id == req.params.id
-	            });
+                user = userList.filter(function(el) {
+                    return el.id == req.params.id
+                });
             
             res.status('200').json(user[0] || []);
         });
@@ -242,8 +242,8 @@ app.route('/adminuser')
         // get userlist from db
         
         redis.get(adminlistObj, function (err, obj) {
-	        userObj = parseJsonList(obj);
-			
+            userObj = parseJsonList(obj);
+            
             res.status(200).json(userObj);
         });
     })
@@ -297,9 +297,9 @@ app.route('/adminuser/:id([0-9]+)')
                 
         redis.get(adminlistObj, function (err, obj) {
             var adminList = parseJsonList(obj),
-            	user = adminList.filter(function(el) {
-	                return el.id == req.params.id
-	            });
+                user = adminList.filter(function(el) {
+                    return el.id == req.params.id
+                });
             
             res.status(200).json(user[0] || []);
         });
@@ -368,15 +368,15 @@ app.route('/organizer')
         // get userlist from db
         
         redis.get(organizerObj, function (err, obj) {
-	        var organizerList = parseJsonList(obj);
-			
+            var organizerList = parseJsonList(obj);
+            
             res.status(200).json(organizerList);
         });
     })
     
     
     .post(function(req, res) {
-        // save new user in db
+        //  save new user in db  
         
         redis.get(organizerObj, function (err, obj) {
             // get old list
@@ -423,9 +423,9 @@ app.route('/organizer/:id([0-9]+)')
                 
         redis.get(organizerObj, function (err, obj) {
             var organizerList = parseJsonList(obj),
-            	organisation = organizerList.filter(function(el) {
-	                return el.id == req.params.id
-	            });
+                organisation = organizerList.filter(function(el) {
+                    return el.id == req.params.id
+                });
             
             res.status(200).json(organisation[0] || []);
         });
@@ -495,8 +495,8 @@ app.route('/event')
         // get userlist from db
         
         redis.get(eventObj, function (err, obj) {
-	        var eventList = parseJsonList(obj);
-			
+            var eventList = parseJsonList(obj);
+            
             res.json(eventList).status('200');
         });
     })
@@ -550,9 +550,9 @@ app.route('/event/:id([0-9]+)')
                 
         redis.get(eventObj, function (err, obj) {
             var eventList = parseJsonList(obj),
-            	event = eventList.filter(function(el) {
-	                return el.id == req.params.id
-	            });
+                event = eventList.filter(function(el) {
+                    return el.id == req.params.id
+                });
             
             res.status(200).json(event[0] || []);
         });
