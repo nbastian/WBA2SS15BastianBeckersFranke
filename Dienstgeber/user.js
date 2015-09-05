946b4bd0d8f8ae83031bae3411eac70e5f328b94
Christian Franke <info@franky.ws>
2015-09-05 14:35:56 +0200

    user.js fix auth

--- Dienstgeber/user.js
+++ Dienstgeber/user.js
@@ -127,29 +127,30 @@ module.exports = {
                 });
             });
 		
-		app.post('/user/authenticate', function(req, res) {
-            redis.get(userlistObj, function (err, obj) {
-                var userList = JSON.parse(obj);
-                for (var i in userList) {
-                    if (userList[i].username == req.body.username) {
-                        if (userList[i].password != sha1sum(req.body.password)) {
-                            return res.json({ success: false, message: 'Authentication failed. Wrong password.'});
-                        } else {
-                            var token = jwt.sign(userList[i], tokenSecret, {
-                                expiresInMinutes: 1440 //24 Stunden
-                            });
-                
-                            return res.json({
-                                success: true,
-                                message: 'Enjoy your token!',
-                                token: token
-                            });
-                        }
-                    }
-                }
-                res.json({ success: false, message: 'Authentication failed. User not found.' });
-            });
-		});  
+		app.route('/user/authenticate')
+			.post(function(req, res) {
+	            redis.get(userlistObj, function (err, obj) {
+	                var userList = JSON.parse(obj);
+	                for (var i in userList) {
+	                    if (userList[i].username == req.body.username) {
+	                        if (userList[i].password != sha1sum(req.body.password)) {
+	                            return res.json({ success: false, message: 'Authentication failed. Wrong password.'});
+	                        } else {
+	                            var token = jwt.sign(userList[i], tokenSecret, {
+	                                expiresInMinutes: 1440 //24 Stunden
+	                            });
+	                
+	                            return res.json({
+	                                success: true,
+	                                message: 'Enjoy your token!',
+	                                token: token
+	                            });
+	                        }
+	                    }
+	                }
+	                res.json({ success: false, message: 'Authentication failed. User not found.' });
+	            });
+			});  
 		        
 		console.log('module user loaded successful');
     }
